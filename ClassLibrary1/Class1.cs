2e765ce4c4892c027c94d79a4bcb5a1cada89343
asinuk-sinyuk <sinuykal@gmail.com>
2016-09-23 13:26:44 +0300

    initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Complex
+    {
+        public double x;
+        public double y;
+
+        public Complex (double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Print()
+        {
+            
+        }
+        
+    }
+
+    public class Operations
+    {
+
+        public static Boolean IsEqual (Complex a, Complex b)
+        {
+            return (a.x == b.x) && (a.y == b.y);
+        }
+
+        public static Complex Add (Complex a, Complex b)
+        {
+            return new Complex(a.x + b.x, a.y + b.y);
+        }
+
+        public static Complex Sub (Complex a, Complex b)
+        {
+            return new Complex(a.x - b.x, a.y - b.y);
+        }
+
+        public static Complex Mul (Complex a, Complex b)
+        {
+            return new Complex((a.x * b.x - a.y*b.y), (a.y * b.x + a.x * b.y));
+        }
+
+        public static Complex Div(Complex a, Complex b)
+        {
+            if ((b.x == 0)&&(b.y == 0))
+            {
+                System.Console.WriteLine("Division by zero!");
+                return null;
+            }
+            else
+            {
+                return new Complex((a.x * b.x + a.y * b.y)/(b.x * b.x + b.y * b.y),
+                    (a.y * b.x - a.x * b.y) / (b.x * b.x + b.y * b.y));
+            }
+        }
+    }
+}
---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,58 @@
+﻿using Calculator;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestEquals()
+        {
+            Complex a = new Complex(1, 2);
+            Complex b = new Complex(1, 2);
+            Assert.AreEqual(true, Operations.IsEqual(a, b));
+        }
+
+        [Test]
+        public void TestAdd()
+        {
+            Complex a = new Complex(1, 2);
+            Complex b = new Complex(2, 6);
+            Complex expected = new Complex(3, 8);
+            Assert.AreEqual(true, Operations.IsEqual(expected, Operations.Add(a, b)));
+        }
+
+        [Test]
+        public void TestSub()
+        {
+            Complex a = new Complex(1, 2);
+            Complex b = new Complex(2, 6);
+            Complex expected = new Complex(-1, -4);
+            Assert.AreEqual(true, Operations.IsEqual(expected, Operations.Sub(a, b)));
+        }
+
+        [Test]
+        public void TestMul()
+        {
+            Complex a = new Complex(1, 2);
+            Complex b = new Complex(2, 6);
+            Complex expected = new Complex(-10, 10);
+            Assert.AreEqual(true, Operations.IsEqual(expected, Operations.Mul(a, b)));
+        }
+
+        [Test]
+        public void TestDiv()
+        {
+            Complex a = new Complex(1, 0);
+            Complex b = new Complex(1, 1);
+            Complex expected = new Complex(0.5, -0.5);
+            Assert.AreEqual(true, Operations.IsEqual(expected, Operations.Div(a, b)));
+        }
+    }
+}
